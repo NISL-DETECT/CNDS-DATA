a8138ce2d2b42eeac958833c7685d43ec99fb871
NISL-DETECT <44659940+NISL-DETECT@users.noreply.github.com>
2018-11-13 20:04:17 +0800

    Update client.py

--- client.py
+++ client.py
@@ -15,9 +15,13 @@ if __name__ == '__main__':
 
     # send some data (in this case a HTTP GET request)
     #client.send('GET /index.html HTTP/1.1\r\nHost: {}.{}\r\n\r\n'.format(sld, tld))
-    #client.send(url.format(sld,tld))
     #You can change the url which you want to test here to detect.
-    client.send('http://www.66kj.com/sx.asp'.format(sld,tld))
+    url = 'www.xxxx.com'
+    #An example of black webpage in Chinese.
+    #client.send('http://www.66kj.com'.format(sld,tld))
+    #An example of white webpage in Chinese.
+    #client.send('http://news.qq.com/world_index.shtml'.format(sld,tld))
+    client.send(url.format(sld,tld))
 
     # receive the response data (4096 is recommended buffer size)
     response = client.recv(4096)
