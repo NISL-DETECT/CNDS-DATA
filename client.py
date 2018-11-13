2cb396c7577e1d448f86a16973950f44fc1be5a8
NISL-DETECT <44659940+NISL-DETECT@users.noreply.github.com>
2018-11-13 11:23:58 +0800

    Add files via upload

---
+++ client.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+import socket
+
+if __name__ == '__main__':
+    hostname, sld, tld, port = 'www', 'integralist', 'co.uk', 80
+    target = '{}.{}.{}'.format(hostname, sld, tld)
+
+    # create an ipv4 (AF_INET) socket object using the tcp protocol (SOCK_STREAM)
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # connect the client
+    # client.connect((target, port))
+    client.connect(('202.112.51.36', 9998))
+
+    # send some data (in this case a HTTP GET request)
+    #client.send('GET /index.html HTTP/1.1\r\nHost: {}.{}\r\n\r\n'.format(sld, tld))
+    #client.send(url.format(sld,tld))
+    #You can change the url which you want to test here to detect.
+    client.send('http://www.66kj.com/sx.asp'.format(sld,tld))
+
+    # receive the response data (4096 is recommended buffer size)
+    response = client.recv(4096)
+
+    print(response)
